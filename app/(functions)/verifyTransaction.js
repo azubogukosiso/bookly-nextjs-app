7634aae05ce533760dc7d471822c7dad33d2a131
azuboguko@gmail.com <azuboguko@gmail.com>
2025-01-02 14:55:49 +0100

    Modify: removed console.log message displaying response

--- app/(functions)/verifyTransaction.js
+++ app/(functions)/verifyTransaction.js
@@ -9,9 +9,7 @@ export const verifyTransaction = async (reference) => {
             body: JSON.stringify({ reference })
         }, { cache: "no-store" });
 
-        console.log("here it is: ", await res.json());
-
-        if (res.ok) return { isVerified: true, message: "Your order has been placed and verified successfully" };
+        if (res.ok) return { isVerified: true, message: "Your order has been verified and placed successfully" };
     } catch (error) {
         return { isVerified: false, message: "Couldn't place the order. Please try again" };
     }
