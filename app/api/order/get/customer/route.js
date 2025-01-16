874ecd5571a9fcc31e2490ffb49657fbae02f77b
azuboguko@gmail.com <azuboguko@gmail.com>
2025-01-16 20:16:50 +0100

    Modify: Remove console logs from customer order retrieval API

--- app/api/order/get/customer/route.js
+++ app/api/order/get/customer/route.js
@@ -5,12 +5,8 @@ import Order from "@/app/(models)/Order.model";
 export async function GET(req) {
     const id = req.nextUrl.searchParams.get("id");
 
-    console.log("THE ID: ", id);
-
     try {
         const orders = await Order.find({ customerId: id });
-
-        console.log("CUS ORDERS: ", orders);
 
         if (orders) return NextResponse.json({ data: orders }, { status: 200 });
     } catch (err) {
